7a650a84272e4f134485b531c2d56b1f662e34b6
simon <simonnfts@gmail.com>
2024-01-03 15:19:55 -0500

    adding address key types for better type inference

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,2 @@
 export * from "./abis";
 export * from "./addresses";
-import {
-  CarbonCreditDescendingPriceAuctionABI,
-  EarlyLiquidityABI,
-  GCCGuardedLaunchABI,
-  GlowGuardedLaunchABI,
-  GovernanceABI,
-  GrantsTreasuryABI,
-  ImpactCatalystABI,
-  MinerPoolAndGCAABI,
-  SafetyDelayABI,
-  USDGABI,
-  VetoCouncilABI,
-} from "./abis";
-
-import { addresses } from "./addresses";
